5c842a225fa247f9173374c4cdf57d09578ba227
Mark Vaughn <iftrueelsefalse@gmail.com>
2017-07-11 13:12:42 +0200

    Initialize endpoint
    Initialize endpoint
    
    Copyright

--- php/src/Snagshout/Promote/Resource/UsersResource.php
+++ php/src/Snagshout/Promote/Resource/UsersResource.php
@@ -19,6 +19,39 @@ class UsersResource extends Resource
     /**
      * 
      *
+     * @param \Snagshout\Promote\Model\InitializeMigrationBody $body 
+     * @param array  $parameters List of parameters
+     * @param string $fetch      Fetch mode (object or response)
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\Snagshout\Promote\Model\Error
+     */
+    public function initialize(\Snagshout\Promote\Model\InitializeMigrationBody $body, $parameters = [], $fetch = self::FETCH_OBJECT)
+    {
+        $queryParam = new QueryParam();
+        $url = '/api/v1/users/migrate';
+        $url = $url . ('?' . $queryParam->buildQueryString($parameters));
+        $headers = array_merge(['Host' => 'localhost'], $queryParam->buildHeaders($parameters));
+        $body = $this->serializer->serialize($body, 'json');
+        $request = $this->messageFactory->createRequest('POST', $url, $headers, $body);
+        $promise = $this->httpClient->sendAsyncRequest($request);
+        if (self::FETCH_PROMISE === $fetch) {
+            return $promise;
+        }
+        $response = $promise->wait();
+        if (self::FETCH_OBJECT == $fetch) {
+            if ('403' == $response->getStatusCode()) {
+                return $this->serializer->deserialize((string) $response->getBody(), 'Snagshout\\Promote\\Model\\Error', 'json');
+            }
+            if ('500' == $response->getStatusCode()) {
+                return $this->serializer->deserialize((string) $response->getBody(), 'Snagshout\\Promote\\Model\\Error', 'json');
+            }
+        }
+
+        return $response;
+    }
+    /**
+     * 
+     *
      * @param \Snagshout\Promote\Model\CheckEmailRequestBody $body 
      * @param array  $parameters List of parameters
      * @param string $fetch      Fetch mode (object or response)
