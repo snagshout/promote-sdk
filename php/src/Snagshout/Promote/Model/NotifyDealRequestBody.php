1a6aa3a47e7963f7d57c7820ac1daa4ecc7a3bac
Mark Vaughn <iftrueelsefalse@gmail.com>
2017-07-24 09:08:57 -0600

    Adds notify endpoint

---
+++ php/src/Snagshout/Promote/Model/NotifyDealRequestBody.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Copyright 2016-2017, Snagshout <developers@snagshout.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This file is part of the Merchant package
+ */
+
+namespace Snagshout\Promote\Model;
+
+class NotifyDealRequestBody
+{
+    /**
+     * @var string
+     */
+    protected $type;
+    /**
+     * @var string
+     */
+    protected $title;
+    /**
+     * @var string
+     */
+    protected $body;
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+    /**
+     * @param string $type
+     *
+     * @return self
+     */
+    public function setType($type = null)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    /**
+     * @param string $title
+     *
+     * @return self
+     */
+    public function setTitle($title = null)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+    /**
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+    /**
+     * @param string $body
+     *
+     * @return self
+     */
+    public function setBody($body = null)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+}
