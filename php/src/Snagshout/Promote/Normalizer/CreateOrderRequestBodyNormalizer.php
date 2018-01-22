6cea1bba3569a8947940b85ea604214ebf805f33
Mark Vaughn <iftrueelsefalse@gmail.com>
2018-01-22 11:37:06 -0700

    Add new classes

---
+++ php/src/Snagshout/Promote/Normalizer/CreateOrderRequestBodyNormalizer.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Copyright 2016-2017, Snagshout <developers@snagshout.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This file is part of the Merchant package
+ */
+
+namespace Snagshout\Promote\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\SerializerAwareNormalizer;
+
+class CreateOrderRequestBodyNormalizer extends SerializerAwareNormalizer implements DenormalizerInterface, NormalizerInterface
+{
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        if ($type !== 'Snagshout\\Promote\\Model\\CreateOrderRequestBody') {
+            return false;
+        }
+
+        return true;
+    }
+    public function supportsNormalization($data, $format = null)
+    {
+        if ($data instanceof \Snagshout\Promote\Model\CreateOrderRequestBody) {
+            return true;
+        }
+
+        return false;
+    }
+    public function denormalize($data, $class, $format = null, array $context = [])
+    {
+        $object = new \Snagshout\Promote\Model\CreateOrderRequestBody();
+        if (property_exists($data, 'email')) {
+            $object->setEmail($data->{'email'});
+        }
+
+        return $object;
+    }
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $data = new \stdClass();
+        if (null !== $object->getEmail()) {
+            $data->{'email'} = $object->getEmail();
+        }
+
+        return $data;
+    }
+}
