122dee3b57b25684c60fc53cabf0136dc4967310
Mark Vaughn <iftrueelsefalse@gmail.com>
2018-03-23 11:43:19 -0600

    Cancel/Confirm Rebates

---
+++ php/src/Snagshout/Promote/Model/ConfirmRebateRequestBody.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Copyright 2016-2017, Snagshout <developers@snagshout.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This file is part of the Merchant package
+ */
+
+namespace Snagshout\Promote\Model;
+
+class ConfirmRebateRequestBody
+{
+    /**
+     * @var string
+     */
+    protected $email;
+    /**
+     * @var string
+     */
+    protected $code;
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    /**
+     * @param string $email
+     *
+     * @return self
+     */
+    public function setEmail($email = null)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+    /**
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+    /**
+     * @param string $code
+     *
+     * @return self
+     */
+    public function setCode($code = null)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+}
---
+++ php/src/Snagshout/Promote/Normalizer/CancelRebateRequestBodyNormalizer.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Copyright 2016-2017, Snagshout <developers@snagshout.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This file is part of the Merchant package
+ */
+
+namespace Snagshout\Promote\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\SerializerAwareNormalizer;
+
+class CancelRebateRequestBodyNormalizer extends SerializerAwareNormalizer implements DenormalizerInterface, NormalizerInterface
+{
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        if ($type !== 'Snagshout\\Promote\\Model\\CancelRebateRequestBody') {
+            return false;
+        }
+
+        return true;
+    }
+    public function supportsNormalization($data, $format = null)
+    {
+        if ($data instanceof \Snagshout\Promote\Model\CancelRebateRequestBody) {
+            return true;
+        }
+
+        return false;
+    }
+    public function denormalize($data, $class, $format = null, array $context = [])
+    {
+        $object = new \Snagshout\Promote\Model\CancelRebateRequestBody();
+        if (property_exists($data, 'email')) {
+            $object->setEmail($data->{'email'});
+        }
+
+        return $object;
+    }
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $data = new \stdClass();
+        if (null !== $object->getEmail()) {
+            $data->{'email'} = $object->getEmail();
+        }
+
+        return $data;
+    }
+}
