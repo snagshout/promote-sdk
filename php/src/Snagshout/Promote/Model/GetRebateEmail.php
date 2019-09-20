70241cba099762f70d4b79abeccb69d84e388aa8
Mark Vaughn <iftrueelsefalse@gmail.com>
2019-09-20 15:46:50 -0600

    New Ones

---
+++ php/src/Snagshout/Promote/Model/GetRebateEmail.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Copyright 2016-2018, Snagshout <developers@snagshout.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This file is part of the Merchant package
+ */
+
+namespace Snagshout\Promote\Model;
+
+class GetRebateEmail
+{
+    /**
+     * @var string
+     */
+    protected $fbUserId;
+    /**
+     * @return string
+     */
+    public function getFbUserId()
+    {
+        return $this->fbUserId;
+    }
+    /**
+     * @param string $fbUserId
+     *
+     * @return self
+     */
+    public function setFbUserId($fbUserId = null)
+    {
+        $this->fbUserId = $fbUserId;
+
+        return $this;
+    }
+}
