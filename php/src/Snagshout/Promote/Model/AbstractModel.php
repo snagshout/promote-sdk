287ce7b58b2964d4cce5dd47696e0c9b44f3f1e7
Andrii Prykhodko <andriichello@gmail.com>
2023-03-08 12:27:25 +0200

    Create AbstractModel

---
+++ php/src/Snagshout/Promote/Model/AbstractModel.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Copyright 2016-2023, Snagshout <developers@snagshout.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This file is part of the Merchant package
+ */
+
+namespace Snagshout\Promote\Model;
+
+/**
+ * AbstractModel class.
+ */
+abstract class AbstractModel
+{
+    //
+}
